845275dd8cda1eace27f44de6ff4b58b10bf0c1d
ioannaoiko <ioannaoiko60@gmail.com>
2022-10-30 12:45:53 +0200

    args

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
+#include <CGAL/convex_hull_2.h>
+#include <CGAL/Convex_hull_traits_adapter_2.h>
+#include <CGAL/property_map.h>
 #include <iostream>
+#include <fstream>
+#include "utils.hpp"
 
 using namespace std;
 
-int main(void){
-    cout<<"hello\n";
+int main(int argc, char* argv[]){
+    if(argc < 9){
+        cout<<"Not enough arguments\n";
+        return -1;
+    }
+    string input_file = argv[2];
+    string output_file = argv[4];
+    string algorithm = argv[6];
+    int edge_selection = atoi(argv[8]);
+    string init;
+    if(argc == 11){
+        init = argv[10];
+    }
+    return 0;               
 }
