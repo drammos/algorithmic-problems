84b65adc92bfa4914d52c5c37a65714d0a54881e
d.rammos <d.rammos@wappier.com>
2022-10-31 18:27:42 +0200

    Add source folder for files

---
+++ src/include/convex_hull.h
@@ -0,0 +1,9 @@
+// #ifndef CONVEX_HULL_H
+// #define CONVEX_HULL_H
+
+
+// #include "lib.h"
+
+// int convex_hull1(vector<Point1>, int);
+
+// #endif
---
+++ src/modules/convex_hull.cpp
@@ -0,0 +1,9 @@
+// #include "convex_hull.h"
+
+// using namespace std;
+
+// int convex_hull1( vector<Point1> points, int edge_selection){
+    
+//     cout << "All is good-convex" << endl;
+//     return 0;
+// }
