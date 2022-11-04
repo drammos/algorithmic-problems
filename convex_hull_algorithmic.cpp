e2ccad3befc5d0d85e29848be1d246f865782aaa
d.rammos <d.rammos@wappier.com>
2022-11-04 23:20:01 +0200

    Corrections in random_edge algorithmic and in convex_hull

--- convex_hull_algorithmic.cpp
+++ convex_hull_algorithmic.cpp
@@ -1,31 +1,8 @@
-#include <iostream>
+#include "convex_hull_algorithmic.h"
 
-using namespace std;
-
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/convex_hull_2.h>
-#include <CGAL/Polygon_2.h>
-#include <fstream>
-#include <time.h>
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef K::Point_2 Point_2;
-typedef std::istream_iterator< Point_2 > point2_iterator;
-typedef std::vector<Point_2> Points;
-typedef std::vector<Point_2>::iterator pveciterator;
-
-typedef CGAL::Polygon_2<K> Polygon_2;
-
-
-
-using namespace std;
-
-typedef K::Segment_2 Segment_2;
-typedef Polygon_2::Vertex_iterator VertexIterator;
-
+// Algorithmic for edge with Min Area
 Polygon_2 min_area(vector<Point_2> internal_points, Polygon_2 polygon){
     
-    srand(time(NULL));
-
     while( !internal_points.empty() ){
         
         
@@ -147,11 +124,9 @@ Polygon_2 min_area(vector<Point_2> internal_points, Polygon_2 polygon){
     return polygon;
 }
 
-
+// Algorithmic for edge with Max Area
 Polygon_2 max_area(vector<Point_2> internal_points, Polygon_2 polygon){
     
-    srand(time(NULL));
-
     while( !internal_points.empty() ){
         
         // max area is MAX 
@@ -268,13 +243,132 @@ Polygon_2 max_area(vector<Point_2> internal_points, Polygon_2 polygon){
     return polygon;
 }
 
+// Algorithmic for random-edge
+Polygon_2 random_edge(vector<Point_2> internal_points, Polygon_2 polygon){
+    
+    srand(time(NULL));
+
+    while( !internal_points.empty() ){
+        
+        bool flag_find_point = false;
+        Segment_2 edge;
+        Point_2 internal_point;
+        int num_internal_point = 0;
+
+        vector<Segment_2> edges;
+        for(const Segment_2& edge  : polygon.edges()){
+            edges.push_back(edge);
+        }
+
+        while( flag_find_point == false){
+
+            int my_rand = rand();
+            int num_edges = polygon.edges().size();;
+            
+            int num_edge = my_rand%num_edges;
+
+            // Take the random edge
+            edge = edges.at(num_edge);
+
+
+            Point_2 p_1 = edge.point(0);
+            Point_2 p_2 = edge.point(1);
+            
+            // min distance is MAX            
+            double min_distance = numeric_limits<double>::max();
+            Point_2 internal_point_to_choise;
+
+            int num_internal_point_to_choise = 0;
+        
+            // I will find the internal point with minimum distance from edge
+            // And I will save it
+            for(int i=0; i < internal_points.size(); i++){
+                Point_2 point = internal_points.at(i);
+
+                bool flag = false;
+
+                Segment_2 seg1(p_1, point);
+                Segment_2 seg2(p_2, point);
+
+                // Check for the edges and internalpoints
+                // Because the polygon must be simple every time
+                for(const Segment_2& edge_ :polygon.edges()){
+
+                    Point_2 first_point = edge_.point(0);
+                    Point_2 second_point = edge_.point(1);
+
+                    // For the same points in differents eges
+                    if( first_point != p_1 && second_point != p_1){
+                        if(CGAL::do_intersect(seg1, edge_) == true){
+                            flag = true;
+                            break;
+                        }
+                    }
+
+                    // For the same points in differents eges
+                    if( first_point != p_2 && second_point != p_2){
+                        if(CGAL::do_intersect(seg2, edge_) == true){
+                            flag = true;
+                            break;
+                        }
+                    }
+
+                }
+
+                // If flag == true continue with next internal point
+                if( flag == true){
+                    continue;
+                }
+
+                // Find the distance
+                double distance = CGAL::squared_distance(edge, point);
+
+                if( distance < min_distance){
+                    flag_find_point = true;
+                    min_distance = distance;
+                    internal_point_to_choise = point;
+                    num_internal_point_to_choise = i;
+                }
+            }   
+        
+            internal_point = internal_point_to_choise;
+            num_internal_point = num_internal_point_to_choise;
+        }
+    
+        // Delete the interal point from vector
+        internal_points.erase( internal_points.begin() + num_internal_point);
+        
+        // Take the point-2 from edge
+        Point_2 p2 = edge.point(1);
+
+        
+        VertexIterator iter;
+        for( 
+            iter = polygon.vertices_begin();
+            iter != polygon.vertices_end();
+            ++iter
+        ){
+            if( iter->x() == p2.x() && iter->y() == p2.y()){
+                break;
+            }
+        }
+
+        // Change the polygon
+        polygon.insert(iter, internal_point);
+
+    }
+    
+
+    return polygon;
+}
+
 
 
 /// @brief
 /// @param points 
 /// @param edge_selection 
 /// @return 
-int convex_hull( vector<Point_2> points, int edge_selection){
+double convex_hull( vector<Point_2> points, int edge_selection){
     
     Points result;
 
@@ -285,18 +379,6 @@ int convex_hull( vector<Point_2> points, int edge_selection){
     Polygon_2 polygon;
     for (pveciterator iter=result.begin(); iter!=result.end(); ++iter){
         polygon.push_back(*iter);
-    }
-
-    std::cout << "\n\nBefore from algorithm:\n\n";
-    
-    // Take the vertices from polyhon
-    for(const Point_2& p : polygon.vertices()){
-        std::cout << "Vertices  " << p << std::endl;
-    }
-    
-    // Take the edges from polygon
-    for(const Segment_2& e  : polygon.edges()){
-        std::cout << "Edge  " << e << std::endl;
     }
 
     // Vector with internals_points
@@ -317,37 +399,37 @@ int convex_hull( vector<Point_2> points, int edge_selection){
         }
     }
 
-    // Print the internal points
-    for(int i = 0; i < internal_points.size(); i++){
-        cout << "Inernal    " << internal_points.at(i) << endl;
-    }
+    
 
     if( edge_selection == 1){
-        // polygon = random_edge(internal_points, polygon);
+        polygon = random_edge(internal_points, polygon);
     }
     else if( edge_selection == 2){
         polygon = min_area(internal_points, polygon);
     }
     else{
-        // polygon = max_area(internal_points, polygon);
+        polygon = max_area(internal_points, polygon);
     }
-    std::cout << "\n\nAfter from algorithm:\n\n";
+    std::cout << "Polyganization";
 
     // Take the vertices from polyhon
-    for(const Point_2& p : polygon.vertices()){
-        std::cout << "Vertices  " << p << std::endl;
+    for(const Point_2& point : polygon.vertices()){
+        std::cout << point << std::endl;
     }
     
     // Take the edges from polygon
-    for(const Segment_2& e  : polygon.edges()){
-        std::cout << "Edge  " << e << std::endl;
+    for(const Segment_2& edge  : polygon.edges()){
+        std::cout << "Edge  " << edge << std::endl;
     }
-    
 
+    cout << "Algorithmic: convex_hull" << endl;
+    
+    cout << "edge_selection: " << edge_selection << endl;
+
+    double area = polygon.area();
     cout << "Polygon is simple:     "<< polygon.is_simple() << endl;
 
 
-
-    return EXIT_SUCCESS;
+    return area;
 
 }
