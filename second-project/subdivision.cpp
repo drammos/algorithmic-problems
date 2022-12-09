1decf561b970feee5cd1bfaa0966bf3f99904e92
d.rammos <d.rammos@wappier.com>
2022-12-09 20:25:12 +0200

    Add subdivision files

--- second-project/subdivision.cpp
+++ second-project/subdivision.cpp
@@ -1,6 +1,28 @@
 #include "subdivision.hpp"
 #include "simulated_annealing.hpp"
 
-Polygon_2 subdivision(){
+using namespace std;
 
+
+/// @brief
+/// @param points 
+/// @param L 
+/// @param min_max 
+/// @param threshold 
+/// @return 
+Polygon_2D subdivision(vector<Point_2> points, int L, string min_max, double threshold){
+    int m = 10;
+    
+    // Sort the points
+    sort(points.begin(), points.end(), check_the_x);
+    // Gia arxi
+    // xorizo se temaxia
+    // opou prepei to aristero x na xei aristera&deksia ena x pio xamilo toulaxtstion
+    // an m dn to ikanopoiei m, m+1, m+...
+    //apothikeuo to struct mou me akmes
+
+    // for
+    Polygon_2D polygon;
+
+    return polygon;
 }
--- second-project/subdivision.hpp
+++ second-project/subdivision.hpp
@@ -1,3 +1,4 @@
 #include "simulated_annealing.hpp"
+using namespace std;
 
-Polygon_2D subdivision();
+Polygon_2D subdivision(vector<Point_2>, int, string, double);
